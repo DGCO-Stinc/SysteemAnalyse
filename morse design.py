87a4a99f8407cea7cfad1bf3e95ce5e53725673e
Joran <joranko@xs4all.nl>
2019-12-03 09:10:00 +0100

    Revert "Revert "prototype""
    This reverts commit a2f4a7a93a8b986888babe414844c804bec2a1ce.

---
+++ morse design.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Morse")
+text1 = tk.Text(root, height=1, width=50)
+text2 = tk.Text(root, height=10, width=50)
+text3 = tk.Text(root, height=1, width=50)
+text4 = tk.Text(root, height=10, width=50)
+b1 = tk.Button(root, text="To morse")
+b2 = tk.Button(root, text="To text")
+speaker = tk.PhotoImage(file="speaker_icon.png")
+label = tk.Label(image=speaker)
+text1.insert(tk.INSERT,"Input")
+text3.insert(tk.INSERT,"Output")
+
+#disable input and output text
+text1.config(state="disabled")
+text3.config(state="disabled")
+text4.config(state="disabled")
+
+
+#packs
+#Input text
+text1.pack()
+#Input
+text2.pack()
+#Speaker image
+label.pack()
+#Output text
+text3.pack()
+#Output
+text4.pack()
+#Buttons
+b1.pack(side=tk.LEFT)
+b2.pack(side=tk.LEFT)
+
+root.mainloop()
