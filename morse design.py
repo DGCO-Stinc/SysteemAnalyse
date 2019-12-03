c11efd1e703308e8a12d3a787fff84a17acebc27
Joran <joranko@xs4all.nl>
2019-12-03 12:55:28 +0100

    morse base added
    Added the base code for the converter

--- morse design.py
+++ morse design.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+morse = ['.-','-...','-.-.-','-..','..-.','--.','....','..','.---','-.-','.-..','--','-.','---','--.','--.-','.-.','...','-','..-','...-','.--','-..-','-.--','--..']
+
+
 root = tk.Tk()
 root.title("Morse")
 text1 = tk.Text(root, height=1, width=50)
@@ -8,7 +12,7 @@ text3 = tk.Text(root, height=1, width=50)
 text4 = tk.Text(root, height=10, width=50)
 b1 = tk.Button(root, text="To morse")
 b2 = tk.Button(root, text="To text")
-speaker = tk.PhotoImage(file="speaker_icon.png")
+speaker = tk.PhotoImage(file="img/speaker_icon.png")
 label = tk.Label(image=speaker)
 text1.insert(tk.INSERT,"Input")
 text3.insert(tk.INSERT,"Output")
