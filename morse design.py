92b848fad37ad7b18baff0fec5fbdc89a68dcceb
Joran <joranko@xs4all.nl>
2019-12-04 09:37:25 +0100

    to morse button fixed
    to morse button is now functional

--- morse design.py
+++ morse design.py
@@ -1,26 +1,33 @@
 import tkinter as tk
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-morse = ['.-','-...','-.-.-','-..','..-.','--.','....','..','.---','-.-','.-..','--','-.','---','--.','--.-','.-.','...','-','..-','...-','.--','-..-','-.--','--..']
-
+def morse_base():
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','0','1','2','3','4','5','6','7','8','9']
+    morse = ['.-','-...','-.-.','-..','.','..-.','--.','....','..','.---','-.-','.-..','--','-.','---','--.','--.-','.-.','...','-','..-','...-','.--','-..-','-.--','--..','/','-----','.----','..---','...--','....-','.....','-....','--...','---..','----.']   
+    text = text2.get()
+    for char in text:
+        converted = morse[alphabet.index(char)]
+        text4.delete('1.0', 'end')
+        text4.insert(tk.INSERT, converted)
+    
 
 root = tk.Tk()
 root.title("Morse")
 text1 = tk.Text(root, height=1, width=50)
-text2 = tk.Text(root, height=10, width=50)
+text2 = tk.Entry(root, width=50)
 text3 = tk.Text(root, height=1, width=50)
 text4 = tk.Text(root, height=10, width=50)
-b1 = tk.Button(root, text="To morse")
+b1 = tk.Button(root, text="To morse", command=morse_base)
 b2 = tk.Button(root, text="To text")
 speaker = tk.PhotoImage(file="img/speaker_icon.png")
 label = tk.Label(image=speaker)
 text1.insert(tk.INSERT,"Input")
 text3.insert(tk.INSERT,"Output")
 
+
 #disable input and output text
 text1.config(state="disabled")
 text3.config(state="disabled")
-text4.config(state="disabled")
+#text4.config(state="disabled")
 
 
 #packs
